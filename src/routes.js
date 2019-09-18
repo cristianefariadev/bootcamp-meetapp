b00241292e44e5f341681efa2c763fa6c7bf32a6
cristiane.trc@gmail.com <cristiane.trc@gmail.com>
2019-09-18 19:44:19 -0300

    criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello rocket' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Cristiane Faria',
+    email: 'cristiane@bitup.com.br',
+    password_hash: '20934839048',
+  });
+  return res.json(user);
 });
+
+// routes.get('/', (req, res) => {
+//   return res.json({ message: 'Hello rocket' });
+// });
 
 export default routes;
